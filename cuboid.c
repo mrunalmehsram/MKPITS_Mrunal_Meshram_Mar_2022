1b946056d780519b9c25c46c7167510d5f21deca
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 18:12:16 +0530

    Add files via upload

---
+++ cuboid.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int c=2,h,l,b;
+	int cuboid;
+	printf("enter value of h,l and b");
+	scanf("%d%d%d",&h,&l,&b);
+	cuboid=c*h*(l+b);
+	printf("%d",cuboid);
+	
+}
