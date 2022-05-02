c8e7f0aef9a9b5974b4baf54f8ff94965098d758
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 18:04:07 +0530

    Add files via upload

---
+++ function fact.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	fun();
+}
+void fun()
+{
+	int num,i;
+	printf("enter the number");
+	scanf("%d",&num);
+	i=num;
+}
+void fact(int num)
+{
+	int fact=1;
+	fact= fact*1;
+	printf("fact %d",fact);
+}
