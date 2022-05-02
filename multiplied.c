a7781c12e6276c9953da86d4e516ccb9a746b250
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 17:51:45 +0530

    Add files via upload

---
+++ multiplied.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,mul;
+	printf("enter the value of num1");
+	scanf("%d",&num1);
+	printf("enter the value of num2");
+	scanf("%d",&num2);
+	mul=num1*num2;
+	printf("multiplication of mul=%d",mul);
+}
