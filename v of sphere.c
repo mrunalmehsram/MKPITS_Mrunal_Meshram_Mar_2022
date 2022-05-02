ea614d27d1cacb087af666a8ae46e59520fce693
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 18:16:53 +0530

    Add files via upload

---
+++ v of sphere.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r;
+	float pi=3.14,c=1.33,V;
+	printf("enter the value of r");
+	scanf("%d",&r);
+	V=1.33*pi*r*r*r;
+	printf("%f",V);
+
+}
