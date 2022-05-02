4a063171e4e26e31a00874b73a2cf38a52c100ab
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 18:02:07 +0530

    Add files via upload

---
+++ 85.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("%d",test(78,95));
+	printf("\n%d",test(25,35));
+	printf("\n%d",test(40,50));
+	printf("\n%d",test(55,60));
+}
+int test (int x,int y)
+{
+	return (x>=40 && x<=50 && y>=40 && y<=50) || (x>=50 && x<=60 && y>=50 && y<=60);
+}
