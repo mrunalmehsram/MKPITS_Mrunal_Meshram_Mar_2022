48b9a337e4894cf0ebde3fff3f37337d26a3423b
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 18:00:48 +0530

    Add files via upload

---
+++ 81.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,i,n2;
+	printf("enter two numbers");
+	scanf("%d%d",&n1,&n2);
+	for(i=100;i<=200;i++)
+	{
+		printf("\n enter number is in the range of 100 to 200");
+	}
+	printf("\n enter number is invalid");
+	printf("\n %d=n1",n1);
+	printf("\n %d=n2",n2);
+}
