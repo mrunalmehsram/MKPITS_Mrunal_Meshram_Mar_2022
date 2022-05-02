e3b141c7950aeeb31bd57923e91349d84a0bbbf9
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 17:57:08 +0530

    Add files via upload

---
+++ function6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("%d",test(53));
+	printf("\n %d",test(30));
+	printf("\n %d",test(51));
+}
+int test(int n)
+{
+	const int x=51;
+	if(n>x)
+	{
+		return (n-x)*3;
+	}
+	return x-n;
+}
