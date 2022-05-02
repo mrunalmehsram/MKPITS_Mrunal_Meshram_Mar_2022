868f76d6d3d379c9d9b7be3c1cf6c005616aa29d
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-02 17:54:09 +0530

    Add files via upload

---
+++ 54.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,a=1,count;
+	while(a<=200)
+	{
+		count=0;
+		i=2;
+		while(i<=a/2)
+		{
+			if(a%i==0)
+			{
+				count ++;
+				break;
+			}
+			i++;
+		}
+		if(count==0&&a!=1)
+		{
+			printf("%d",a);
+		}
+		i++;
+	}
+}
