a15faa6b921d07d2207015b93f52e32a9cc82897
mrunalmehsram <101799398+mrunalmehsram@users.noreply.github.com>
2022-05-06 14:05:25 +0530

    Add files via upload

---
+++ three  number greater then or smaller.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("%d",test(1,2,3));
+	printf("\n%d",test(4,5,6));
+	printf("\n%d",test(-1,1,0));
+}
+int test (int x,int y,int z)
+{
+	return x<y && y<z;
+}
